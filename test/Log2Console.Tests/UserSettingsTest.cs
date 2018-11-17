bd91e2f7efd1dff779c264fe966d66c4e3b9e283
Pablo Escobar <pescobar@facturecolombia.com>
2018-11-17 08:21:06 -0500

    Folder reorganization

---
+++ test/Log2Console.Tests/UserSettingsTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Log2Console.Tests
+{
+    [TestFixture]
+    public class UserSettingsTest
+    {
+
+        [Test]
+        public void GetSettings()
+        {
+
+        }
+    }
+}
